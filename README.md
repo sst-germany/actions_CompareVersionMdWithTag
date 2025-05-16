8df966c89961963b9ed781c027f201340dd4aed1
uwekli <uwe.klingelhoeller@sst-germany.de>
2025-05-16 10:11:33 +0200

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # actions_CompareVersionMdWithTag
+
+This action will read the content of the first line of the "VERSION.md" file and compare it to
+the tag name.
+
+The tag name is expected to be of scheme "releases/v{version}".
+
+If both are equal, the workflow proceeds, otherwise it is terminated with EXIT.
---
+++ main.ps1
@@ -0,0 +1,18 @@
+# Get name of the Git-Tag
+$tagName = $env:GITHUB_REF_NAME
+Write-Host "Git-Tag: $tagName"
+
+# Get Version from VERSION.md
+$markdownVersion = Get-Content -Path "VERSION.md" | Select-Object -First 1
+Write-Host "VERSION.md: $markdownVersion"
+
+# Check tag scheme:
+if ($tagName -notlike "releases/v*") {
+    Write-Error "Git tag does not start with 'releases/v': $tagName"
+    exit 1
+}
+
+if ($tagName -notmatch "^releases/v$markdownVersion$") {
+    Write-Error "Git tag does not match the expected format 'releases/v<markdownVersion>': $tagName"
+    exit 1
+}
